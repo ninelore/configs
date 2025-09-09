dfb0fc32362167668c3c6b179d5cd3648359801e
Ingo Reitz <9l@9lo.re>
2025-09-09 04:15:52 +0200

    nixos: greetd useTextGreeter

--- nixos/desktop/niri.nix
+++ nixos/desktop/niri.nix
@@ -20,6 +20,7 @@
       dbus.packages = with pkgs; [ swayosd ];
       greetd = {
         enable = true;
+        useTextGreeter = true;
         settings = {
           default_session = {
             command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --kb-power 1";
