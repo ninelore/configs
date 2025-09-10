1c10c2a342bc430ef08fb48b892552fff7e91c92
Ingo Reitz <9l@9lo.re>
2025-09-10 12:48:08 +0200

    hm: add supergfxctl aliases

--- hm/cli/sh.nix
+++ hm/cli/sh.nix
@@ -8,6 +8,9 @@ let
     "q" = "exit";
     "untar" = "tar -xavf";
     "v" = "nvim";
+    "sgfx0" = "supergfxctl -m Integrated";
+    "sgfx1" = "supergfxctl -m Hybrid";
+    "sgfx" = "supergfxctl -S";
   };
 in
 {
