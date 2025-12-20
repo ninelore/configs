cd355aa2492d3d84257e19089c3240f9611ad6ef
Ingo Reitz <9l@9lo.re>
2025-12-20 05:29:10 +0100

    nixos: use nix-community.cachix.org

--- nixos/default.nix
+++ nixos/default.nix
@@ -26,9 +26,10 @@ in
         experimental-features = "nix-command flakes";
         auto-optimise-store = mkDefault true;
         trusted-users = [
-          "root"
           "@wheel"
         ];
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       };
     };
 
