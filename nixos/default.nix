88add986c3f4b23b4df98d60d0751a4eac095f2d
Ingo Reitz <9l@9lo.re>
2026-01-09 13:50:47 +0100

    nixos: remove workaround overlay

--- nixos/default.nix
+++ nixos/default.nix
@@ -37,11 +37,7 @@ in
     };
 
     nixpkgs.overlays = [
-      (final: prev: {
-        lrzsz = prev.lrzsz.overrideAttrs (oldAttrs: {
-          env.NIX_CFLAGS_COMPILE = "-Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-error=incompatible-pointer-types -std=gnu17";
-        });
-      })
+      # (final: prev: { })
     ];
 
     boot = {
