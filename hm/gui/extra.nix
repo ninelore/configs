8abc92f4203985e54fd3228a552ec1e263c1abc5
Ingo Reitz <9l@9lo.re>
2026-03-13 22:08:32 +0100

    hm: extra: add vesktop

--- hm/gui/extra.nix
+++ hm/gui/extra.nix
@@ -6,17 +6,15 @@
 }:
 {
   config = lib.mkIf config.ninelore.extraApps {
-    home.packages =
-      with pkgs;
-      [
-        ausweisapp
-        quasselClient
-        signal-desktop
-        # tuba
-      ]
-      ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
-        discord-canary
-      ];
+    home.packages = with pkgs; [
+      ausweisapp
+      quasselClient
+      signal-desktop
+    ];
+
+    programs.vesktop = {
+      enable = true;
+    };
 
     services.protonmail-bridge.enable = true;
   };
