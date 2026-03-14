db75eec4581deb1dae1f0cded15eeed95257f759
Ingo Reitz <9l@9lo.re>
2026-03-14 17:39:08 +0100

    hm: extra: readd discord-canary

--- hm/gui/extra.nix
+++ hm/gui/extra.nix
@@ -6,11 +6,16 @@
 }:
 {
   config = lib.mkIf config.ninelore.extraApps {
-    home.packages = with pkgs; [
-      ausweisapp
-      quasselClient
-      signal-desktop
-    ];
+    home.packages =
+      with pkgs;
+      [
+        ausweisapp
+        quasselClient
+        signal-desktop
+      ]
+      ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
+        discord-canary
+      ];
 
     programs.vesktop = {
       enable = true;
