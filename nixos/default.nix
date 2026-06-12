87bacd17f192c15fa58a89585ecb0efd2d8ccaab
Ingo Reitz <9l@9lo.re>
2026-06-12 06:35:56 +0200

    nixos: add broken package workaround overlay

--- nixos/default.nix
+++ nixos/default.nix
@@ -38,10 +38,17 @@
 
     nixpkgs.overlays = [
       # (final: prev: { })
-      # FIXME 2026-04-26: openldap test failure, still present 2026-05-02
       (final: prev: {
+        # FIXME 2026-04-26: openldap test failure, still present 2026-05-02
         openldap = prev.openldap.overrideAttrs (_: {
           doCheck = !prev.stdenv.hostPlatform.isi686;
+        });
+        # FIXME 2026-06-12: binwalk binPath dep, missing build dep.
+        # Fixed in next channel update.
+        uefi-firmware-parser = prev.uefi-firmware-parser.overridePythonAttrs (_: {
+          build-system = [
+            prev.python3Packages.setuptools-scm
+          ];
         });
       })
     ];
