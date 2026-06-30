e1c10e241b583cd8871421d048fbb73716d4b63d
Ingo Reitz <9l@9lo.re>
2026-06-30 07:48:59 +0200

    overlays: drop obsolete workarounds

--- overlays.nix
+++ overlays.nix
@@ -6,19 +6,6 @@
       neovim-unwrapped = prev.neovim-unwrapped.override { wasmSupport = true; };
     })
     # Fixes
-    (final: prev: {
-      # FIXME 2026-04-26: openldap test failure, still present 2026-05-02
-      openldap = prev.openldap.overrideAttrs (_: {
-        doCheck = !prev.stdenv.hostPlatform.isi686;
-      });
-      # FIXME 2026-06-12: binwalk binPath dep, missing build dep.
-      # Fixed in next channel update.
-      uefi-firmware-parser = prev.uefi-firmware-parser.overridePythonAttrs (_: {
-        build-system = [
-          prev.python3Packages.setuptools-scm
-        ];
-      });
-    })
     # (final: prev: { })
   ];
 }
