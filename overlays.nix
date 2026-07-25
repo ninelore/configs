0b58a04ccad79917acf2e78e61b062795ab785fa
Ingo Reitz <9l@9lo.re>
2026-07-25 14:29:12 +0200

    overlays: drop file workaround

--- overlays.nix
+++ overlays.nix
@@ -6,23 +6,6 @@
       neovim-unwrapped = prev.neovim-unwrapped.override { wasmSupport = true; };
     })
     # Fixes
-    (final: prev: {
-      python314Packages = prev.python314Packages.overrideScope (
-        pyFinal: pyPrev: {
-          patool = pyPrev.patool.override {
-            # Fix in staging, backport for patool
-            # https://github.com/NixOS/nixpkgs/pull/540742
-            file = prev.file.overrideAttrs {
-              postPatch = ''
-                substituteInPlace src/landlock.c --replace-fail \
-                  "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR" \
-                  "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR | LANDLOCK_ACCESS_FS_EXECUTE"
-              '';
-            };
-          };
-        }
-      );
-    })
     # (final: prev: { })
   ];
 }
