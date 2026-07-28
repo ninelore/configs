30fa140d607004aa203ca548b2d7c4c8c02d6d40
Ingo Reitz <9l@9lo.re>
2026-07-28 22:47:24 +0200

    overlays: add workaround for niri dependency failure

--- overlays.nix
+++ overlays.nix
@@ -6,6 +6,19 @@
       neovim-unwrapped = prev.neovim-unwrapped.override { wasmSupport = true; };
     })
     # Fixes
+    (final: prev: {
+      # TODO: drop with next nixpkgs bump
+      libdisplay-info = prev.libdisplay-info.overrideAttrs (finalAttrs: {
+        version = "0.3.0";
+        src = prev.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "emersion";
+          repo = "libdisplay-info";
+          rev = finalAttrs.version;
+          sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+        };
+      });
+    })
     # (final: prev: { })
   ];
 }
